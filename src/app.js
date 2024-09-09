9101838afe75c312353e07fcc35d7b3bd7653810
KOTHECODE <160642762+KOTHECODE@users.noreply.github.com>
2024-09-09 23:47:19 +0000

    complete

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
-/* eslint-disable */
+/* eslint-disable no-console */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function phrase() {
+    let who = ["the dog", "my leg", "uncle", "The Traffic"];
+    let what = ["slowed", "jammed up", "got shakey", "did not pick up"];
+    let when = [
+      "even though I was late",
+      "right in time",
+      "when I need it the most",
+      "during my down time",
+      "while I was so far"
+    ];
+
+    let part = Math.floor(Math.random() * who.length);
+
+    return who[part] + " " + what[part] + " " + when[part];
+  }
+
+  // Ensure there is an element with the id 'message' in your HTML
+  const messageElement = document.getElementById("message");
+  if (messageElement) {
+    messageElement.innerHTML = "<h1>Sorry I'm late but, " + phrase() + "</h1>";
+  } else {
+    console.log('Element with id "message" not found.');
+  }
 };
